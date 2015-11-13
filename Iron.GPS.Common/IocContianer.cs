1fc348677e677d6aa0a4d84ec6a0f42c863d5528
Iron Tang <iron.tang@beyondsoft.com>
2015-11-13 17:54:47 +0800

    added Name: Version: 1.0.0.0 Jira ID: SalesForce ID: N/A Author: Iron.Tang Reviewer: Iron.Tang Unit Test:Y Meet Code Standard:Y

---
+++ Iron.GPS.Common/IocContianer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iron.GPS.Common
+{
+    public sealed class IocContianer
+    {
+        private static IUnityContainer _container;
+
+        private static object[] _obj = new object[] { };
+
+        public static IUnityContainer InnerContainer
+        {
+            get
+            {
+                lock (_obj)
+                {
+                    if (null == _container)
+                    {
+                        StartUp();
+                    }
+                }
+
+                return _container;
+            }
+        }
+
+        private static void StartUp()
+        {
+            _container = new UnityContainer();
+            _container.LoadConfiguration();
+
+            ////or
+            //UnityConfigurationSection configure = ConfigurationManager.GetSection(UnityConfigurationSection.SectionName) as UnityConfigurationSection;
+            //if (configure != null)
+            //{
+            //    configure.Configure(_container);
+            //}
+        }
+    }
+}
