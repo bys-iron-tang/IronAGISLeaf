0b084b0a6e4596a0b9806c36bbe25ec43b5610d9
Iron Tang <iron.tang@beyondsoft.com>
2015-12-01 17:31:36 +0800

    ASDF

--- Iron.GPS.Application/Scripts/ScriptModels/main.js
+++ Iron.GPS.Application/Scripts/ScriptModels/main.js
@@ -6,6 +6,15 @@
         'angularAMD': '../angularAMD.min',
         'ui-bootstrap': '../angular-ui/ui-bootstrap-tpls',
         'blockUI': '../angular-block-ui.min',
+        'ngload': '../ngload.min',
+        'angular-sanitize': '../angular-sanitize',
+    },
 
-    }
+    //add angular modules that  does not support AMD out of box, put them in a shim
+    shim: {
+        'angularAMD': ['angular'],
+        'angular-route': ['angular'],
+    },
+
+    deps: ['']
 });
