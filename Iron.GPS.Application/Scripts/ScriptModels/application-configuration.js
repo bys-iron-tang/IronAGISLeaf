339a0e3928aef5b35efacb2d6747e1eeb61f4164
Iron Tang <iron.tang@beyondsoft.com>
2015-12-15 18:06:06 +0800

    adsf

--- Iron.GPS.Application/Scripts/ScriptModels/application-configuration.js
+++ Iron.GPS.Application/Scripts/ScriptModels/application-configuration.js
@@ -1,1 +1,34 @@
 ﻿'use strict'
+define(['angularAMD', 'angular-route', 'ui-bootstrap', 'angular-sanitize', 'blockUI', ], function (angularAMD) {
+    var app = angular.module("mainModule", ['ngRoute', 'blockUI', 'ngSanitize', 'ui.bootstrap']);
+
+    app.filter("leadingZeros", function () {
+        return function (data) {
+            var pad = "000" + data;
+            pad = pad.substr(pad.length - 3);
+            return pad;
+        }
+    });
+
+    app.config(function ($httpProvider) {
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        $httpProvider.defaults.withCredentials = true;
+    });
+
+    app.config(function (blockUIConfigProvider) {
+        blockUIConfigProvider.message("loading.....");
+        blockUIConfigProvider.autoBlock(false);
+    });
+
+    var indexController = function ($scope, $rootScope, $http, $location, blockUI) {
+
+    };
+
+    indexController.$inject = ['$scope', '$rootScope', '$http', '$location', 'blockUI'];
+
+    app.controller("indexController", indexController);
+
+    angularAMD.bootstrap(app);
+
+    return app;
+})
--- Iron.GPS.Application/Scripts/ScriptModels/main.js
+++ Iron.GPS.Application/Scripts/ScriptModels/main.js
@@ -1,20 +1,25 @@
 ﻿require.config({
+    baseUrl: '../../Scripts/ScriptModels',
     paths: {
         'application-configuration': 'application-configuration',
-        'angular': '../angular.min',
+        'angular': '../angular',
         'angular-route': '../angular-route.min',
         'angularAMD': '../angularAMD.min',
         'ui-bootstrap': '../angular-ui/ui-bootstrap-tpls',
-        'blockUI': '../angular-block-ui.min',
+        'blockUI': '../angular-block-ui',
         'ngload': '../ngload.min',
-        'angular-sanitize': '../angular-sanitize',
+        'angular-sanitize': '../angular-sanitize'
     },
 
     //add angular modules that  does not support AMD out of box, put them in a shim
     shim: {
         'angularAMD': ['angular'],
         'angular-route': ['angular'],
+        'blockUI': ['angular'],
+        'angular-sanitize': ['angular'],
+        'ui-bootstrap':['angular']
     },
 
-    deps: ['']
+    //kick start application
+    deps: ['application-configuration']
 });
